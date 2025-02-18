764838a588278c2d3ce2b6c7540728828bafdb4e
Michael Woolnough <mw31@sanger.ac.uk>
2025-02-18 13:07:35 +0000

    Correct embed script to not export JS idents

--- analytics/embed.sh
+++ analytics/embed.sh
@@ -7,7 +7,7 @@
 	cat style.css | tr -d '\n	';
 	echo -n "</style>";
 	echo -n "<script type=\"module\">";
-	jspacker -i "/$(grep "<script" -n index.html | sed -e 's/.*src="\([^"]*\)".*/\1/')" | terser -m  --module --compress pure_getters,passes=3 --ecma 2020 | tr -d '\n';
+	jspacker -i "/$(grep "<script" index.html | sed -e 's/.*src="\([^"]*\)".*/\1/')" -n | terser -m  --module --compress pure_getters,passes=3 --ecma 2020 | tr -d '\n';
 	echo -n "</script>";
 	tail -n3 index.html | tr -d '\n	';
 ) > "index.html";
