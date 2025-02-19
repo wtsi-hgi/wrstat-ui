30fd63ebd4cb173d6e6f1fc0be296197e3bf9835
Michael Woolnough <mw31@sanger.ac.uk>
2025-02-19 13:51:05 +0000

    Add docs

--- cmd/clean.go
+++ cmd/clean.go
@@ -39,8 +39,18 @@ var (
 
 var cleancmd = &cobra.Command{
 	Use:   "clean",
-	Short: "clean filter basedir history to only contain paths with a certain prefix",
-	Long:  ``,
+	Short: "clean basedir history of invalid history",
+	Long: `clean filters basedir history to only have paths with a certain prefix
+
+Legacy basedirectory databases may have history relating to other mountpoints
+which is no longer valid with the new database structures.
+
+This command will remove all history except for those whose paths match the
+prefix supplied in the --prefix/-p flag.
+
+The --view/-v flag can be used to view the keys that would have be removed if
+the flag were not supplied.
+`,
 	Run: func(_ *cobra.Command, args []string) {
 		if len(args) != 1 {
 			die("need a basedirs db path")
