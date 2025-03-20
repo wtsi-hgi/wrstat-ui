8ce00210db7f616a7a568a42c2feae015c06c9eb
Michael Woolnough <mw31@sanger.ac.uk>
2025-03-20 14:39:55 +0000

    Plot total syscalls

--- syscalls/src/code.ts
+++ syscalls/src/code.ts
@@ -36,6 +36,7 @@ type syscalls = {
 	bytes: number;
 	closes: number;
 	stats: number;
+	syscalls: number;
 };
 
 type EventSyscall = syscalls & {
@@ -75,16 +76,16 @@ const amendNode = (node: Element, propertiesOrChildren: PropertiesOrChildren, ch
 
 	return d.toLocaleDateString() + " " + d.toLocaleTimeString()
       },
-      keys: (keyof syscalls)[] = ["opens", "reads", "closes", "stats", "bytes"],
-      keyColours = ["#0f0", "#00f", "#f80", "#f0f", "#f00"],
-      newSyscall = (time = 0) => ({time, "opens": 0, "reads": 0, "bytes": 0, "closes": 0, "stats": 0}),
+      keys: (keyof syscalls)[] = ["syscalls", "opens", "reads", "closes", "stats", "bytes"],
+      keyColours = ["#000", "#0f0", "#00f", "#f80", "#f0f", "#f00"],
+      newSyscall = (time = 0) => ({time, "opens": 0, "reads": 0, "bytes": 0, "closes": 0, "stats": 0, "syscalls": 0}),
       range = function* <V>(start: number, stop: number, fn: (n: number) => V) {
 	for (let i = start; i <= stop; i++) {
 		yield fn(i);
 	}
       },
       maxY = 200,
-      buildChart = (events: EventSyscall[], startMinute: number, endMinute: number) => {
+      buildMinuteByMinute = (events: EventSyscall[], startMinute: number, endMinute: number) => {
 	const minutes = new Map(range(startMinute, endMinute,  n => [n, newSyscall(n)])),
 	      files = new Map<string, syscalls>();
 
@@ -100,21 +101,41 @@ const amendNode = (node: Element, propertiesOrChildren: PropertiesOrChildren, ch
 			}
 
 			const lastTime = last[key] === 0 ? now - 1 : last.time / 60 | 0,
-			      dt = now - lastTime;
+			      dt = now - lastTime,
+			      v = event[key] / dt;
 
 			for (const i of range(lastTime + 1, now, n => n)) {
-				minutes.get(i)![key] += (event[key] ?? 0) / dt;
+				const m = minutes.get(i)!;
+
+				m[key] += v;
+
+				if (key !== "bytes") {
+					m.syscalls += v
+				}
 			}
 		}
 	}
 
-	const lines = keys.map(() => [] as [number, number][]),
-	      maxes = keys.map(() => 0);
+	return minutes;
+      },
+      findDataMaxes = (events: EventSyscall[], startMinute: number, endMinute: number) => {
+	const minutes = buildMinuteByMinute(events, startMinute, endMinute),
+	      maxes: [number, number] = [0, 0];
+	
+	for (const [, minute] of minutes) {
+		maxes[0] = Math.max(maxes[0], minute.syscalls);
+		maxes[1] = Math.max(maxes[1], minute.bytes);
+	}
+
+	return maxes;
+      },
+      buildChart = (events: EventSyscall[], startMinute: number, endMinute: number, maxSyscalls: number, maxBytes: number) => {
+	const minutes = buildMinuteByMinute(events, startMinute, endMinute),
+	      lines = keys.map(() => [] as [number, number][]);
 	
 	for (const [, minute] of minutes) {
 		for (const [n, key] of keys.entries()) {
 			lines[n].push([minute.time, minute[key]]);
-			maxes[n] = Math.max(maxes[n], minute[key]);
 		}
 	}
 
@@ -124,12 +145,10 @@ const amendNode = (node: Element, propertiesOrChildren: PropertiesOrChildren, ch
 	      }
 	}
 
-	const maxSyscalls = Math.max(...maxes.slice(0, 4));
-
 	return svg({"viewBox": `0 0 ${endMinute - startMinute + 20} ${maxY + 100}`}, [
 		g({"transform": "translate(10 5)"}, [
-			lines.slice(0, 4).map((points, l) => polyline({"points": points.map(([n, point]) => `${n - startMinute},${maxY - maxY * point / maxSyscalls}`).join(" "), "stroke": keyColours[l], "fill": "none"})),
-			polyline({"points": lines[4].map(([n, point]) => `${n - startMinute},${maxY - maxY * point / maxes[4]}`).join(" "), "stroke": keyColours[4], "fill": "none"}),
+			lines.slice(0, 5).map((points, l) => polyline({"points": points.map(([n, point]) => `${n - startMinute},${maxY - maxY * point / maxSyscalls}`).join(" "), "stroke": keyColours[l], "fill": "none"})),
+			polyline({"points": lines[5].map(([n, point]) => `${n - startMinute},${maxY - maxY * point / maxBytes}`).join(" "), "stroke": keyColours[5], "fill": "none"}),
 			line({"y1": maxY + "", "y2": maxY + "", "x2": "100%", "stroke": "#000"}),
 			Array.from(range(Math.ceil(startMinute / 60), Math.floor(endMinute / 60), n => text({"transform": `translate(${n * 60 - startMinute}, ${maxY + 5}) rotate(90) scale(0.5)`}, formatTime(n * 3600))))
 		])
@@ -137,7 +156,10 @@ const amendNode = (node: Element, propertiesOrChildren: PropertiesOrChildren, ch
       },
       showErr = (err: EventError) => li(`${formatTime(err.time)}, ${err.file} (${err.host}): ${err.message}`),
       display = (run: string, data: Data) => {
-	let {opens, reads, bytes, closes, stats} = data.events.reduce((d, e) => (d.opens += e.opens ?? 0, d.reads += e.reads ?? 0, d.bytes += e.bytes ?? 0, d.closes += e.closes ?? 0, d.stats += e.stats ?? 0, d), newSyscall());
+	const {opens, reads, bytes, closes, stats} = data.events.reduce((d, e) => (d.opens += e.opens ?? 0, d.reads += e.reads ?? 0, d.bytes += e.bytes ?? 0, d.closes += e.closes ?? 0, d.stats += e.stats ?? 0, d), newSyscall()),
+	      dataStart = data.events[0].time / 60 | 0,
+	      dataEnd = data.events.at(-1)!.time / 60 | 0,
+	      [maxSyscalls, maxBytes] = findDataMaxes(data.events, dataStart, dataEnd);
 
 	clearNode(body, [
 		h2(run),
@@ -170,12 +192,12 @@ const amendNode = (node: Element, propertiesOrChildren: PropertiesOrChildren, ch
 				summary("Charts"),
 				div([
 					select({"change": function(this: HTMLSelectElement) {
-						(this.nextSibling as Element).replaceWith(buildChart(this.value === "..." ? data.events : data.events.filter(event => event.host === this.value), data.events[0].time / 60 | 0, data.events.at(-1)!.time / 60 | 0));
+						(this.nextSibling as Element).replaceWith(buildChart(this.value === "..." ? data.events : data.events.filter(event => event.host === this.value), dataStart, dataEnd, maxSyscalls, maxBytes));
 					}}, [
 						option({"value": "..."}, "-- All Hosts --"),
 						Array.from(data.events.reduce((hosts, event) => (hosts.add(event.host), hosts), new Set<string>())).map(host => option(host))
 					]),
-					buildChart(data.events, data.events[0].time / 60 | 0, data.events.at(-1)!.time / 60 | 0),
+					buildChart(data.events, dataStart, dataEnd, maxSyscalls, maxBytes),
 					ul({"class": "graphKey"}, keys.map((key, n) => li([span({"style": "background-color: " + keyColours[n]}), span(key)])))
 				])
 			])
