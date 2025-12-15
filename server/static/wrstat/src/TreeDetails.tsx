524d909b6179c1ac2460b3fc77c10a32f20d3cbd
Prasannakumar Nuniganti <pn10@sanger.ac.uk>
2025-12-15 14:15:24 +0000

    Add Most common Access/modified time to TreeDetails component

--- server/static/wrstat/src/TreeDetails.tsx
+++ server/static/wrstat/src/TreeDetails.tsx
@@ -29,47 +29,95 @@ import type { Child } from "./rpc";
 import { formatBytes, formatDate, formatNumber } from "./format";
 import type React from "react";
 
-const TreedetailsComponent = ({ details, ...rest}: { details: Child | null} & React.HTMLAttributes<HTMLDivElement>) => {
-	if (!details) {
-		return <></>;
-	}
+const TreedetailsComponent = ({
+  details,
+  ...rest
+}: { details: Child | null } & React.HTMLAttributes<HTMLDivElement>) => {
+  if (!details) {
+    return <></>;
+  }
 
-	return <div id="details" {...rest}>
-		{details.path}
-		<table>
-			<caption>Path Details</caption>
-			<tbody>
-				<tr>
-					<th>Size</th>
-					<td>{formatBytes(details.size)}</td>
-				</tr>
-				<tr>
-					<th aria-label="Files & Directories">Files &amp; Dirs</th>
-					<td>{formatNumber(details.count)}</td>
-				</tr>
-				<tr>
-					<th>Accessed</th>
-					<td>{details.atime ? formatDate(details.atime): ""}</td>
-				</tr>
-				<tr>
-					<th>Modifed</th>
-					<td>{details.mtime ? formatDate(details.mtime): ""}</td>
-				</tr>
-				<tr>
-					<th>Groups</th>
-					<td><div>{details.groups?.join(", ") ?? ""}</div></td>
-				</tr>
-				<tr>
-					<th>Users</th>
-					<td><div>{details.users?.join(", ") ?? ""}</div></td>
-				</tr>
-				<tr>
-					<th>Filetypes</th>
-					<td><div>{details.filetypes?.join(", ") ?? ""}</div></td>
-				</tr>
-			</tbody>
-		</table>
-	</div>;
+  const ageRangeLabel = (r?: number) => {
+    if (r === undefined || r === null) return "";
+
+    switch (r) {
+      case 0:
+        return "> 7 years";
+      case 1:
+        return "5 – 7 years";
+      case 2:
+        return "3 – 5 years";
+      case 3:
+        return "2 – 3 years";
+      case 4:
+        return "1 – 2 years";
+      case 5:
+        return "6 – 12 months";
+      case 6:
+        return "6 – 12 months";
+      case 7:
+        return "1 – 2 months";
+      case 8:
+        return "< 1 month";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <div id="details" {...rest}>
+      {details.path}
+      <table>
+        <caption>Path Details</caption>
+        <tbody>
+          <tr>
+            <th>Size</th>
+            <td>{formatBytes(details.size)}</td>
+          </tr>
+          <tr>
+            <th aria-label="Files & Directories">Files &amp; Dirs</th>
+            <td>{formatNumber(details.count)}</td>
+          </tr>
+          <tr>
+            <th>Accessed</th>
+            <td>
+              {details.atime ? formatDate(details.atime) : ""}
+              {details.common_atime !== undefined && (
+                <> (most commonly {ageRangeLabel(details.common_atime)} ago)</>
+              )}
+            </td>
+          </tr>
+          <tr>
+            <th>Modifed</th>
+            <td>
+              {details.mtime ? formatDate(details.mtime) : ""}
+              {details.common_mtime !== undefined && (
+                <> (most commonly {ageRangeLabel(details.common_mtime)} ago)</>
+              )}
+            </td>
+          </tr>
+          <tr>
+            <th>Groups</th>
+            <td>
+              <div>{details.groups?.join(", ") ?? ""}</div>
+            </td>
+          </tr>
+          <tr>
+            <th>Users</th>
+            <td>
+              <div>{details.users?.join(", ") ?? ""}</div>
+            </td>
+          </tr>
+          <tr>
+            <th>Filetypes</th>
+            <td>
+              <div>{details.filetypes?.join(", ") ?? ""}</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TreedetailsComponent;
