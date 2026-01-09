04f29520c87e157c7d9a8fdb94e897c46d7586f5
Sendu Bala <sb10@sanger.ac.uk>
2026-01-09 18:00:29 +0000

    NewDGUTAWriter (metadata enforcement + snapshot switch on Close())

---
+++ clickhouse/dguta_writer_test.go
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Genome Research Ltd.
+ *
+ * Authors:
+ *   Sendu Bala <sb10@sanger.ac.uk>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+package clickhouse
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/wtsi-hgi/wrstat-ui/db"
+	internaltest "github.com/wtsi-hgi/wrstat-ui/internal/test"
+)
+
+func TestClickHouseDGUTAWriter(t *testing.T) {
+	Convey("DGUTAWriter enforces required metadata", t, func() {
+		os.Setenv("WRSTAT_ENV", "test")
+		Reset(func() { os.Unsetenv("WRSTAT_ENV") })
+
+		th := newClickHouseTestHarness(t)
+		cfg := th.newConfig()
+		cfg.QueryTimeout = 2 * time.Second
+
+		w, err := NewDGUTAWriter(cfg)
+		So(err, ShouldBeNil)
+		So(w, ShouldNotBeNil)
+		Reset(func() { So(w.Close(), ShouldBeNil) })
+
+		paths := internaltest.NewDirectoryPathCreator()
+		err = w.Add(db.RecordDGUTA{Dir: paths.ToDirectoryPath("/"), GUTAs: nil})
+		So(err, ShouldNotBeNil)
+
+		w.SetMountPath("/mnt/test/")
+		w.SetUpdatedAt(time.Date(2026, 1, 9, 12, 0, 0, 0, time.UTC))
+		err = w.Add(db.RecordDGUTA{Dir: paths.ToDirectoryPath("/"), GUTAs: nil})
+		So(err, ShouldBeNil)
+	})
+
+	Convey("DGUTAWriter switches the active snapshot on Close", t, func() {
+		os.Setenv("WRSTAT_ENV", "test")
+		Reset(func() { os.Unsetenv("WRSTAT_ENV") })
+
+		th := newClickHouseTestHarness(t)
+		cfg := th.newConfig()
+		cfg.QueryTimeout = 2 * time.Second
+
+		w, err := NewDGUTAWriter(cfg)
+		So(err, ShouldBeNil)
+		So(w, ShouldNotBeNil)
+
+		const mountPath = "/mnt/test/"
+
+		updatedAt := time.Date(2026, 1, 9, 12, 0, 0, 0, time.UTC)
+
+		w.SetMountPath(mountPath)
+		w.SetUpdatedAt(updatedAt)
+		So(w.Close(), ShouldBeNil)
+
+		conn := th.openConn(cfg.DSN)
+
+		Reset(func() { So(conn.Close(), ShouldBeNil) })
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		rows, err := conn.Query(ctx,
+			"SELECT toString(snapshot_id), updated_at FROM wrstat_mounts_active WHERE mount_path = ?",
+			mountPath,
+		)
+		So(err, ShouldBeNil)
+
+		defer func() { _ = rows.Close() }()
+
+		So(rows.Next(), ShouldBeTrue)
+
+		var (
+			gotSID       string
+			gotUpdatedAt time.Time
+		)
+
+		So(rows.Scan(&gotSID, &gotUpdatedAt), ShouldBeNil)
+
+		expectedSID := snapshotID(mountPath, updatedAt)
+		So(gotSID, ShouldEqual, expectedSID.String())
+		So(gotUpdatedAt, ShouldEqual, updatedAt)
+	})
+}
